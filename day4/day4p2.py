104cb78b5916fe2c78c9c33a2a954abef018266e
Elena Bechmanis <97773024+e-bechmanis@users.noreply.github.com>
2022-12-04 16:18:29 +0000

    done day4 challenge

--- day4/day4p2.py
+++ day4/day4p2.py
@@ -7,9 +7,8 @@ with open("day4input.txt", "r") as file:
             elves_final = [elf.split("-") for elf in elves]
             elf1 = set(range(int(elves_final[0][0]), int(elves_final[0][1]) + 1))
             elf2 = set(range(int(elves_final[1][0]), int(elves_final[1][1]) + 1))
-            elf1_common = elf1.intersection(elf2)
-            elf2_common = elf2.intersection(elf1)
-            if len(elf1_common) == len(elf2) or len(elf2_common) == len(elf1):
+            elves_common = elf1.intersection(elf2)
+            if len(elves_common) > 0:
                 total_errors += 1
 
 print(total_errors)
