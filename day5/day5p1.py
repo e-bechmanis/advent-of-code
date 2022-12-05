4c0aeefcba68a70c938084a66d92a0c566b3887a
Elena Bechmanis <97773024+e-bechmanis@users.noreply.github.com>
2022-12-05 19:25:57 +0000

    finished p1

--- day5/day5p1.py
+++ day5/day5p1.py
@@ -1,44 +1,3 @@
-# This class represents a queue. I will be pushing items to the back of the queue and popping them from the front
-class Queue:
-    def __init__(self, capacity=10):
-        self.the_queue = [None] * capacity
-        self.start = 0
-        self.back = 0
-        self.slots_used = 0
-        self.cap = capacity
-
-    def grow(self):
-        newcap = self.cap * 2
-        newqueue = [None] * newcap
-        i = self.start
-        j = 0
-        while j < self.slots_used:
-            newqueue[j] = self.the_queue[i]
-            i = (i + 1) % self.cap
-            j += 1
-        self.start = 0
-        self.back = self.slots_used
-        self.cap = newcap
-        self.the_queue = newqueue
-
-    def enqueue(self, v):
-        if self.slots_used == self.cap:
-            self.grow()
-        self.the_queue[self.back] = v
-        self.back = (self.back + 1) % self.cap
-        self.slots_used += 1
-
-    def dequeue(self):
-        if self.slots_used != 0:
-            self.start = (self.start + 1) % self.cap
-            self.slots_used -= 1
-
-    def front(self):
-        if self.slots_used > 0:
-            return self.the_queue[self.start]
-        return None
-
-
 with open("day5input.txt", "r") as file:
     stacks = []
     instructions = []
@@ -48,18 +7,21 @@ with open("day5input.txt", "r") as file:
             break
         stacks.append(line.strip())
 
-    # Making sure the schema outlined in input file is transformed into queues
-    schema = [Queue() for i in range((len(stacks[0]) // 4) + 1)]
+    # Making sure the schema outlined in input file is transformed into stacks
+    schema = [[] for i in range((len(stacks[0]) // 4) + 1)]
     for stack in stacks:
         i = 1
         j = 0
         while i < len(stack):
             if stack[i] != " ":
-                schema[j].enqueue(stack[i])
+                schema[j].append(stack[i])
             i += 4
             j += 1
 
-    # Reading instructions into array
+    for each in schema:
+        each.reverse()
+
+    # Keep reading file to save instructions into array
     for line in file:
         temp = line.strip().replace("move ", "").split(" from ")
         temp2 = [str.split(" to ") for str in temp]
@@ -67,3 +29,11 @@ with open("day5input.txt", "r") as file:
 
     # Making moves
     for instruction in instructions:
+        for i in range(int(instruction[0])):
+            arrlen = len(schema[int(instruction[1]) - 1])
+            temp = schema[int(instruction[1]) - 1][arrlen - 1]
+            schema[int(instruction[1]) - 1].pop()
+            schema[int(instruction[2]) - 1].append(temp)
+
+    for queue in schema:
+        print(queue[len(queue) - 1], end="")
